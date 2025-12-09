37c56235028f8c3ae8ef8267144899fdb877b611
Vadia-Jade <missnaturalfuzz4319@gmail.com>
2025-12-09 11:24:33 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Weather from "./Weather";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>
+        Hello world
+      </h1>
+      <Weather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
